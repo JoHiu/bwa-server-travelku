857de73e106681c3dd5c8fa87958d42b3858201e
JoHiu <80262385+JoHiu@users.noreply.github.com>
2021-06-14 13:26:43 +0700

    Last

--- app.js
+++ app.js
@@ -27,6 +27,7 @@ const apiRouter = require("./routes/api");
 
 var app = express();
 
+app.use(cors());
 // view engine setup
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
@@ -56,11 +57,11 @@ app.use("/users", usersRouter);
 app.use("/admin", adminRouter);
 app.use("/api/v1/member", apiRouter);
 
-app.use(cors());
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
   next(createError(404));
 });
+
 // error handler
 app.use(function (err, req, res, next) {
   // set locals, only providing error in development
